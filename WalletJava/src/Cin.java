289716085f2e2fbfd85182cc501823c4ee68b131
Mirado <hei.mirado@gmail.com>
2023-12-09 16:30:19 +0300

    feat:add cin and visit card object

---
+++ WalletJava/src/Cin.java
@@ -0,0 +1,10 @@
+public class Cin {
+    String cinNumber;
+    String owner;
+    
+    public Cin(String cinNumber, String owner){
+        this.cinNumber = cinNumber;
+        this.owner = owner;
+    }
+
+}
---
+++ WalletJava/src/VisitCard.java
@@ -0,0 +1,9 @@
+public class VisitCard {
+    String societyName;
+    String owner;
+
+    public VisitCard(String societyName, String owner){
+        this.societyName = societyName;
+        this.owner = owner;
+    }
+}
