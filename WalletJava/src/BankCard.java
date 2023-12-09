f887d6684404f5a1ca71f8d26aa4a65eb4235c23
Mirado <hei.mirado@gmail.com>
2023-12-09 16:36:45 +0300

    feat:add bank card object

---
+++ WalletJava/src/BankCard.java
@@ -0,0 +1,10 @@
+public class BankCard {
+    String owner;
+    String bank;
+
+    public BankCard(String owner, String bank){
+        this.owner = owner;
+        this.bank = bank;
+    }
+    
+}
