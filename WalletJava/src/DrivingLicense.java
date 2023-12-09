8df4977c2a21278767f62510f3ea9f67b854d561
Mirado <hei.mirado@gmail.com>
2023-12-09 16:45:51 +0300

    feat:add driving license object

---
+++ WalletJava/src/DrivingLicense.java
@@ -0,0 +1,9 @@
+public class DrivingLicense {
+    String owner;
+    String type;
+
+    public DrivingLicense(String owner, String type){
+        this.owner = owner;
+        this.type = type;
+    }
+}
