91bdc17ef957285b1ad93ca50734810fb5850369
Mirado <hei.mirado@gmail.com>
2023-12-10 22:33:09 +0300

    add new object to main

--- WalletJava/src/App.java
+++ WalletJava/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Wallet wallet = new Wallet("mirado");
+        wallet.simulation();
     }
 }
